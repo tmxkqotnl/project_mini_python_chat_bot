f4bb3e756da583c81bad92cfc0872893895a651e
tmxkqotnl <tmxkqotnl@gmail.com>
2022-02-06 15:23:33 +0900

    etc, refac

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('',include('bot.urls')),
+    path('chat/',include('bot.urls')),
     path('admin/', admin.site.urls),
-    path('chat/',include('bot.urls'))
+    
 ]
