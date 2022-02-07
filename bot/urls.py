cd177a3abbc9306c89060936ad6430d5f0b56bd8
tmxkqotnl <tmxkqotnl@gmail.com>
2022-02-07 11:14:13 +0900

    fix, set url

--- bot/urls.py
+++ bot/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
-from bot.views import login,chat_room
+from bot.views import login,chat_room,user_info
 
 urlpatterns = [ 
     path('' , login.Login.as_view() , name='login'),
-    path('chatroom',chat_room.ChatRoom.as_view(),name='chat')
+    path('chatroom',chat_room.ChatRoom.as_view(),name='chat'),
+    path('chatroom/user_info',user_info.UserInfo.as_view(),name='info')
 ]
