7c377e97679a3e0d41f947c3a9712cd53af1e48c
tmxkqotnl <tmxkqotnl@gmail.com>
2022-02-07 11:14:23 +0900

    etc, reset

--- bot/websocket/consumers.py
+++ bot/websocket/consumers.py
@@ -24,7 +24,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
