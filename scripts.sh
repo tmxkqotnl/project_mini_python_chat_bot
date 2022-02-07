d369cdde3f7ba224f9f3af96af893c9c277c40e9
tmxkqotnl <tmxkqotnl@gmail.com>
2022-02-07 09:11:43 +0900

    fix, add docker command

--- scripts.sh
+++ scripts.sh
@@ -2,12 +2,18 @@
 
 if [ "$1" == "server" ]
 then
-    python manage.py runserver 8080
+    python manage.py runserver "$2"
 elif [ "$1" == "migration" ]
 then
     python manage.py makemigrations bot
     wait
     python manage.py migrate
+elif [ "$1" == "up" ]
+then
+    docker-compose up -d
+elif [ "$1" == "down" ]
+then
+    docker-compose down -v
 else
     echo "no command found"
 fi
