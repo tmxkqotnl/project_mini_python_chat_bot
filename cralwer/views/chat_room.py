842ddb4380de77466ed76acbf74b9839dca0a2a1
tmxkqotnl <tmxkqotnl@gmail.com>
2022-02-08 19:24:36 +0900

    fix, 쿼리 처리중

--- cralwer/views/chat_room.py
+++ cralwer/views/chat_room.py
@@ -1,14 +1,14 @@
+from json import loads
 from django.shortcuts import render
 from django.views import View
 from django.core.handlers.asgi import ASGIRequest
-from django.utils import timezone
-from uuid import uuid4,UUID
-from cralwer.const import CRAWLING_START
+from cralwer.const import CATEGORIES, CRAWLING_START
+
 from cralwer.models.searched import Searched
-
-from cralwer.models.user import User
 from cralwer.models.token import Token
+from cralwer.models.query import Query
 
+from uuid import uuid4
 
 class ChatRoom(View):
     def get(self, req: ASGIRequest):
@@ -18,12 +18,8 @@ class ChatRoom(View):
         )
 
     def post(self, req: ASGIRequest):
-        message = req.POST.get("message")
+        message = loads(req.POST.get("message"))
         user_token = Token.objects.get(token=req.session['user_token'])
-        
-        create_dt = timezone.localtime()
-        
-        # if message[0] == CRAWLING_START:
         
         Searched.objects.create(
                 id=uuid4(),
@@ -31,5 +27,18 @@ class ChatRoom(View):
                 user_pk=user_token.user_pk
         )
         
-
+        if message[0] == CRAWLING_START and message.__len__()>1:
+            categories = message[1:]
+            
+            for i in range(len(categories),len(CATEGORIES)):
+                categories = categories + [None ]
+                
+            Query.objects.create(
+                id = uuid4(),
+                user_pk = user_token.user_pk,
+                large=categories[0],
+                medium=categories[1],
+                small=categories[2]
+            )
+        
         return render(req,'cralwer/chatroom.html')
